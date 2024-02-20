8890df9107957012a3be79d2d322c2a61869b2cf
Kylan Metzner <kylan.metzner@audencia.com>
2024-02-20 14:59:04 +0100

    exo1 tasks completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,18 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  # Defines the root path route ("/")
-  # root "posts#index"
+  # index
+  get '/tasks', to: 'tasks#index'
+  # new
+  get '/tasks/new', to: 'tasks#new'
+  # create
+  post '/tasks', to: 'tasks#create'
+  # edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # update
+  patch '/tasks/:id', to: 'tasks#update'
+  # delete
+  delete '/tasks/:id', to: 'tasks#destroy'
+  # show
+  get '/tasks/:id', to: 'tasks#show', as: :task
 end
